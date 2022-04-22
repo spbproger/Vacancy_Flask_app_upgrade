aa014481b76d7296fe4d0ac84d5c28def472936b
spbproger <zemli2016@yandex.ru>
2022-04-23 02:59:13 +0300

    Change directory

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from flask import Flask, request, render_template
-
+from utils import load_candidates_list_from_json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def page_index():
-
-    page_content = """ 
-    <h1>Смотри, это — моя страничка</h1>
-    <p>Страничек много, но эта – моя!</p>
-    <p>Фронтенд – мой лучший друг!</p>
-    """
-    return page_content
+@app.route('/a')
+def index():
+    lc = load_candidates_list_from_json()
+    return render_template("list.html", lc=lc)
 
 
 app.run()
