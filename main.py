151b2efdcbebf3eb577f83856ad3848358703389
spbproger <zemli2016@yandex.ru>
2022-04-23 07:05:06 +0300

    Change directory

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, render_template
-from utils import load_candidates_list_from_json
+from utils import load_candidates_list_from_json, get_candidate
 
 app = Flask(__name__)
 
+candidates = load_candidates_list_from_json()
 
-@app.route('/a')
+
+@app.route('/')
 def index():
-    lc = load_candidates_list_from_json()
-    return render_template("list.html", lc=lc)
+    return render_template("list.html", candidates=candidates)
+
+
+@app.route('/candidates/<candidate_id>/')
+def search_by_id(candidate_id):
+    # item = get_candidate(candidates, candidate_id)
+    # return render_template("single.html", item=item)
+    return render_template("single.html", candidates=candidates, candidate_id=candidate_id)
+
+
+@app.route('/search/<candidate_name>/')
+def search_by_name(candidate_name):
+    return render_template("search.html", candidates=candidates, candidate_name=candidate_name)
+
+
+@app.route('/search/<candidate_name>')
+def search_by_skills(candidate_skills):
+    return render_template("skill.html", candidates=candidates, candidate_name=candidate_skills)
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -22,58 +22,31 @@ def load_candidates_list(candidates_list):
     return candidates
 
 
-def get_candidate(candidate_id):
+def get_candidate(candidates, candidate_id):
     """возвращает одного кандидата по его id"""
-    candidates = load_candidates_list_from_json()
-    candidate = '<pre>\n'
     for item in candidates:
         if item["id"] == candidate_id:
-            candidate += (f'Имя кандидата - {item["name"]}\n'
-                          f'Позиция кандидата - {item["picture"]}\n'
-                          f'Позиция кандидата - {item["position"]}\n'
-                          f'Позиция кандидата - {item["gender"]}\n'
-                          f'Позиция кандидата - {item["age"]}\n'
-                          f'Навыки - {item["skills"]}\n')
-    candidate += '</pre>'
-    return candidate
+            return item
 
 
-def get_candidates_by_name(candidate_name):
+def get_candidates_by_name(candidates, candidate_name):
     """возвращает кандидатов по имени"""
-    candidates = load_candidates_list_from_json()
-    candidate = '<pre>\n'
     for item in candidates:
         if item["name"] == candidate_name:
-            candidate += (f'Имя кандидата - {item["name"]}\n'
-                          f'Позиция кандидата - {item["picture"]}\n'
-                          f'Позиция кандидата - {item["position"]}\n'
-                          f'Позиция кандидата - {item["gender"]}\n'
-                          f'Позиция кандидата - {item["age"]}\n'
-                          f'Навыки - {item["skills"]}\n')
-    candidate += '</pre>'
-    return candidate
+            return item
 
 
-def get_candidates_by_skill(skill_name):
+def get_candidates_by_skill(candidates, skill_name):
     """возвращает кандидатов по навыку"""
-    candidates = load_candidates_list_from_json()
-    candidate = '<pre>\n'
     for item in candidates:
         if skill_name.lower() in item["skills"].lower():
-            candidate += (f'Имя кандидата - {item["name"]}\n'
-                          f'Позиция кандидата - {item["picture"]}\n'
-                          f'Позиция кандидата - {item["position"]}\n'
-                          f'Позиция кандидата - {item["gender"]}\n'
-                          f'Позиция кандидата - {item["age"]}\n'
-                          f'Навыки - {item["skills"]}\n\n')
-    candidate += '</pre>'
-    return candidate
+            return item
 
 # print(load_candidates_list_from_json("candidates.json"))
 #
 # print(load_candidates_list(load_candidates_list_from_json("candidates.json")))
 #
-# print(get_candidate(5))
+# print(get_candidate(load_candidates_list_from_json(), 5))
 #
 # print(get_candidates_by_name("Day Holloway"))
 #
